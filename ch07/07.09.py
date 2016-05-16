3bcd61358ed01a112fda75d6029b11f54f7cccbd
LeO_wqHyw <wqhyw2@gmail.com>
2016-05-16 16:05:57 +0800

    7.5 and 7.10 unfinished.

--- ch07/07.09.py
+++ ch07/07.09.py
@@ -10,4 +10,16 @@ def tr(srcstr, dststr, source):
     @rtype: string
     """
 
-    # TODO
+    start = source.find(srcstr)
+    if start != -1: end = start + len(srcstr)
+    left = source[:start]
+    right = source[end:]
+    return left+dststr+right
+
+
+def test():
+    print tr('abce', 'mno', 'abcedef')
+
+
+if __name__ == '__main__':
+    test()
